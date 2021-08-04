e85af2ecbd47fcc1e93fc499aa17cf1d85d6b73b
Felix Huang <felixhuang12@gmail.com>
2021-08-04 17:24:37 -0400

    Fixes on coordination between popup and background scripts for proper display

--- background.js
+++ background.js
@@ -1,33 +1,71 @@
-let timerTime;
-let counter;
+let timerTime, fixedTime, fixedBreak, periodType, counter;
+let flag = false;
+
+chrome.storage.sync.get(['key'], result => {
+    fixedTime = result.key.sessionTime;
+    fixedBreak = result.key.brTime;
+    console.log(`${fixedTime} and ${fixedBreak}`);
+});
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.msg === 'start-timer'){
         timerTime = message.num;
-        timer2(timerTime);
+        periodType = 'Study Period';
+        notif(timerTime);
+    }
+    else if (message.msg === 'reset'){
+        timerTime = null;
+        counter = null;
+        clearInterval(counter);
+        periodType = '';
+        console.log('reset');
     }
     else if (message.msg === 'get-time'){
-        sendResponse({num: timerTime});
+        sendResponse({num: timerTime, period: periodType});
     }
     else if (message.msg === 'stop-timer'){
         clearInterval(counter);
+        console.log('stop');
     }
     else if (message.msg === 'resume-timer'){
-        timer2(timerTime);
-    }
-    else {
-        return null;
+        timerTime = message.num;
+        notif(timerTime);
     }
     return true;
 });
 
-function timer2(time){
+function notif(time){
+    clearInterval(counter);
     if (time > 0){
         counter = setInterval(() => {
             timerTime = time;
             time--;
             if (time === 0){
                 clearInterval(counter);
+                if (!flag){
+                    chrome.notifications.create({
+                        type: 'basic',
+                        iconUrl: '/icons/icon-128.png',
+                        title: 'Break Time',
+                        message: 'Break coming up!'
+                    });
+                    flag = true;
+                    periodType = 'Break Period';
+                    setTimeout(() => {notif(fixedBreak)}, 1000);
+                    console.log(`${fixedBreak}`);
+                }
+                else {
+                    chrome.notifications.create({
+                        type: 'basic',
+                        iconUrl: '/icons/icon-128.png',
+                        title: 'Study Time',
+                        message: 'Another session coming up!'
+                    });
+                    flag = false;
+                    periodType = 'Study Period';
+                    setTimeout(() => {notif(fixedTime)}, 1000);
+                    console.log(`${fixedTime}`);
+                }
                 timerTime = null;
             }
         }, 1000)
--- popup.js
+++ popup.js
@@ -1,25 +1,60 @@
+let totalTime, count, sessTime, breakTime, sessType;
+const timeSettings = {};
+let breakFlag = false;
 let startStop = document.querySelector('#startStop');
 let reset = document.querySelector('#reset');
 let option1 = document.querySelector('#option1');
 let option2 = document.querySelector('#option2');
-option1.addEventListener('click', () => initialDisplay(5));
-option2.addEventListener('click', () => initialDisplay(60));
-let min;
-let sec;
-let count;
+
+option1.addEventListener('click', function() {
+    initialSettings(1500, 300);
+});
+
+option2.addEventListener('click', () => {
+    initialSettings(3000, 600);
+});
+
+reset.addEventListener('click', () => {
+    getInitialTime();
+    document.getElementById("startStop").innerHTML = 'Start';
+    chrome.runtime.sendMessage({msg: 'reset'});
+    clearInterval(count);
+})
+
+function initialSettings(sessTime, brkTime){
+    timeSettings.sessionTime = sessTime;
+    timeSettings.brTime = brkTime;
+    totalTime = sessTime;
+    storeTime();
+    getInitialTime();
+}
+
+function storeTime(){
+    chrome.storage.sync.set({key: timeSettings}); 
+}
+
+function getInitialTime(){
+    chrome.storage.sync.get(['key'], result => {
+        let min = Math.floor(result.key.sessionTime/60);
+        totalTime = result.key.sessionTime;
+        breakTime = result.key.brTime;
+        sessTime = totalTime;
+        document.getElementById("timer").innerHTML = `${min}:00`;
+        document.getElementById("label").innerHTML = 'Study Period';
+    });
+}
 
 chrome.runtime.sendMessage({msg: 'get-time'}, response => {
     if (response.num){
         timer(response.num);
         document.getElementById("startStop").innerHTML = "Stop";
+        sessType = response.period;
+        document.getElementById("label").innerHTML = `${sessType}`;
+    }
+    else {
+        getInitialTime();
     }
 });
-
-function initialDisplay(time){
-    min = Math.floor((time/60));
-    sec = time;
-    document.getElementById("timer").innerHTML = `${min}:00`;
-}
 
 function begin(time){
     chrome.runtime.sendMessage({msg: 'start-timer', num: time});
@@ -31,7 +66,7 @@ startStop.addEventListener('click', startStopHandler);
 function startStopHandler(){
     let temp = document.getElementById("startStop").innerHTML;
     if (temp === "Start"){
-        begin(sec);
+        begin(totalTime);
         document.getElementById("startStop").innerHTML = 'Stop';
     }
     else if (temp === "Stop"){
@@ -40,32 +75,36 @@ function startStopHandler(){
         chrome.runtime.sendMessage({msg: 'stop-timer'});
     }
     else if (temp === "Resume"){
-        timer(sec);
+        chrome.runtime.sendMessage({msg: 'resume-timer', num: totalTime});
+        timer(totalTime);
         document.getElementById("startStop").innerHTML = 'Stop';
-        chrome.runtime.sendMessage({msg: 'resume-timer'});
     }
     else {
         return null;
     }
 }
-reset.addEventListener('click', () => {
-    initialDisplay(1500);
-    document.getElementById("startStop").innerHTML = 'Start';
-})
 
 function timer(time){
     if (time > 0){
         count = setInterval(() => {
             let minutesLeft = Math.floor(time/60);
             let secondsLeft = Math.floor(time%60);
-            min = minutesLeft;
-            sec = secondsLeft;
+            totalTime = time;
             time--;
             if (minutesLeft < 0 && secondsLeft < 0){
                 clearInterval(count);
-                setTimeout(() => {
-                    alert('Break time! Click');
-                }, 500);
+                if (!breakFlag){
+                    sessType = 'Break Period';
+                    document.getElementById("label").innerHTML = `${sessType}`;
+                    setTimeout(() => {begin(breakTime)}, 1000);
+                    breakFlag = true;
+                }
+                else {
+                    sessType = 'Study Period';
+                    document.getElementById("label").innerHTML = `${sessType}`;
+                    setTimeout(() => {begin(sessTime)}, 1000);
+                    breakFlag = false;
+                }  
             }
             else if (secondsLeft < 10){
                 document.getElementById("timer").innerHTML = `${minutesLeft}:0${secondsLeft}`;
